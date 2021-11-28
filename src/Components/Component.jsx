8bcbdf3a1cb0fc63480bca7efee8e27d451920db
Vlada650 <vladano46@gmail.com>
2021-11-28 11:42:58 +0300

    app

--- src/Components/Component.jsx
+++ src/Components/Component.jsx
@@ -1,20 +1,20 @@
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import '../styles/Component.css';
-import Easy from './Easy'
+import Easy from './Eay'
 import Normal from './Normal'
 import Hard from './Hard'
 
-const Component = ({modes}) => {
- 
+const Component = ({ modes }) => {
+
   const [val, setValue] = useState('Choose your mode');
-  const[elem, showElem] = useState(false);
+  const [elem, showElem] = useState(false);
 
   const handleChange = (e) => {
-    setValue( e.target.value);
+    setValue(e.target.value);
   }
 
-  return ( <>
-   <section className='settings'>
+  return (<>
+    <section className='settings'>
       <p>Let's start!</p>
       <select onChange={handleChange} value={val}>
         <option value="Choose your mode" disabled hidden>Choose your mode</option>
@@ -23,13 +23,13 @@ const Component = ({modes}) => {
         <option value='hardMode' >hardMode</option>
       </select>
       <button onClick={() => showElem(true)}>Start</button>
-   </section>
-   { elem && <section className="main"> 
-    {val === 'easyMode' && <Easy field={modes['easyMode']['field']} />}
-    {val === 'normalMode' && <Normal field={modes['normalMode']['field']} />}
-    {val === 'hardMode' && <Hard field={modes['hardMode']['field']} />}
-  </section>}
-    </>
+    </section>
+    {elem && <section className="main">
+      {val === 'easyMode' && <Easy field={modes['easyMode']['field']} />}
+      {val === 'normalMode' && <Normal field={modes['normalMode']['field']} />}
+      {val === 'hardMode' && <Hard field={modes['hardMode']['field']} />}
+    </section>}
+  </>
   );
 }
 
