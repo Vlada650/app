9f7e2095becb52813a88b9d18cf6a0eecb19d820
Vlada650 <vladano46@gmail.com>
2021-11-28 11:41:04 +0300

    app

--- src/Components/Normal.jsx
+++ src/Components/Normal.jsx
@@ -1,40 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import shortid from 'shortid';
 
-export default function Normal ({field}) {
+function cycle({ fieldNumb, changeBackground }) {
+  let newArray = [];
+
+  for (let i = 0; i < fieldNumb; i++) {
+    newArray.push(React.createElement("div", {
+      "className": "cell",
+      onMouseOver: changeBackground,
+      key: shortid.generate()
+    }))
+  }
+  return newArray;
+}
+
+export default function Easy({ field }) {
   let fieldNumb = Math.pow(field, 2);
-  const[array, setArray] = useState([]); 
+  const [array, setArray] = useState([]);
 
   useEffect(() => {
-    cycle();
- }, []);
+    setArray(cycle({ fieldNumb, changeBackground }));
+  }, []);
 
-function changeBackground(e) {
-    if( e.target.style.background === 'blue') {
+  function changeBackground(e) {
+    if (e.target.style.background === 'blue') {
       e.target.style.background = 'white';
     } else {
       e.target.style.background = 'blue';
     }
-}
-
-function cycle () {
-  let newArray =[];
-  for(let i = 0; i < fieldNumb; i++) {
-    if(array.length > fieldNumb-1){
-      break
-    } else {
-      newArray.push(React.createElement("div",{
-        "className" : "cell",
-        onMouseOver : changeBackground,
-        key : shortid.generate()
-      }))
-    }
-  }   
-  setArray(newArray)
-}
+  }
 
   return (<>
-          <p>Hover a square on middle-level</p>  
-          <div className='normal-mode'>{array}</div>
-        </>
-)}
+    <p>Hover a square on normal-level</p>
+    <div className='normal-mode'>{array}</div>
+  </>
+  )
+}
--- src/Components/easy.jsx
+++ src/Components/easy.jsx
@@ -1,40 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import shortid from 'shortid';
 
-export default function Easy ({field}) {
-  let fieldNumb = Math.pow(field, 2);
-  const[array, setArray] = useState([]); 
+function cycle({ fieldNumb, changeBackground }) {
+  let newArray = [];
 
-  useEffect(() => {
-    cycle();
- }, []);
-
- function changeBackground(e) {
-  if( e.target.style.background === 'blue') {
-    e.target.style.background = 'white';
-  } else {
-    e.target.style.background = 'blue';
+  for (let i = 0; i < fieldNumb; i++) {
+    newArray.push(React.createElement("div", {
+      "className": "cell",
+      onMouseOver: changeBackground,
+      key: shortid.generate()
+    }))
   }
+  return newArray;
 }
 
-  function cycle () {
-    let newArray =[];
-    for(let i = 0; i < fieldNumb; i++) {
-      if(array.length > fieldNumb-1){
-        break
-      } else {
-        newArray.push(React.createElement("div",{
-          "className" : "cell",
-          onMouseOver : changeBackground,
-          key : shortid.generate()
-        }))
-      }
-    }   
-    setArray(newArray)
+export default function Easy({ field }) {
+  let fieldNumb = Math.pow(field, 2);
+  const [array, setArray] = useState([]);
+
+  useEffect(() => {
+    setArray(cycle({ fieldNumb, changeBackground }));
+  }, []);
+
+  function changeBackground(e) {
+    if (e.target.style.background === 'blue') {
+      e.target.style.background = 'white';
+    } else {
+      e.target.style.background = 'blue';
+    }
   }
 
   return (<>
-          <p>Hover a square on easy-level</p>  
-          <div className='easy-mode'>{array}</div>
-        </>
-)}
+    <p>Hover a square on easy-level</p>
+    <div className='easy-mode'>{array}</div>
+  </>
+  )
+}
